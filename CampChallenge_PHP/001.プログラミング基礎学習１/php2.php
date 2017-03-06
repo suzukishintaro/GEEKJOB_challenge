d64ae0091fbf2087b9ccd0ebd11cb51312c62de8
SHINTAROPC\shintaro <suzuki.s.0617@gmail.com>
2017-03-06 15:28:52 +0900

    switch文

---
+++ CampChallenge_PHP/001.プログラミング基礎学習１/php2.php
@@ -0,0 +1,17 @@
+<?php
+$num = 7;
+$massage = ' ';
+ switch ($num){
+  case 1:
+    $massage = 'one';
+    break;
+  case 2:
+    $massage = 'two';
+    break;
+
+  default:
+  $massage = '想定外';
+    break;
+}
+echo $massage;
+ ?>
