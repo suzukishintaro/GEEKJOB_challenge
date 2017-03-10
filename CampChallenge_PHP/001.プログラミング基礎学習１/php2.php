1a0c1c847b071abe34b507d777012edc49d3b0cb
SHINTAROPC\shintaro <suzuki.s.0617@gmail.com>
2017-03-10 13:15:52 +0900

    配列の変更

--- CampChallenge_PHP/001.プログラミング基礎学習１/php2.php
+++ CampChallenge_PHP/001.プログラミング基礎学習１/php2.php
@@ -1,8 +1,8 @@
 <pre>
 <?php
 $var = array("10", "100", "soeda", "hayashi", "-20", "118", "END") ;
-echo $var[3];
-echo "<br>";
-echo implode(" ", $var);
+echo $var[2];
+$var[2] = "33";
+echo $var[2];
 ?>
 </pre>
