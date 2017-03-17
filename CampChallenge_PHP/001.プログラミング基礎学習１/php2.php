9a46db62a30ca51a7d9b7c8a2377c950cf3e40b6
SHINTAROPC\shintaro <suzuki.s.0617@gmail.com>
2017-03-17 11:49:29 +0900

    文字数の取得

--- CampChallenge_PHP/001.プログラミング基礎学習１/php2.php
+++ CampChallenge_PHP/001.プログラミング基礎学習１/php2.php
@@ -1,13 +1,3 @@
 <?php
-echo "日本語をバイト数で取得", strlen('スズキ');
-echo "<br>";
-
-echo "日本語を文字数で取得", mb_strlen('スズキ');
-echo "<br>";
-
-echo "英数字をバイト数で取得", strlen('suzuki');
-echo "<br>";
-
-echo "英数字を文字数で取得", mb_strlen('suzuki');
-echo "<br>";
+echo strstr('suzuki.s.0617@gmail.com', '@');
  ?>
