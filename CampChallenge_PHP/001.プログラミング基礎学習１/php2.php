db378bf88abe1cfdb18f5f1849c2c7a3ac2153b1
SHINTAROPC\shintaro <suzuki.s.0617@gmail.com>
2017-03-17 15:25:26 +0900

    文字の入れ替え

--- CampChallenge_PHP/001.プログラミング基礎学習１/php2.php
+++ CampChallenge_PHP/001.プログラミング基礎学習１/php2.php
@@ -1,3 +1,11 @@
 <?php
-echo strstr('suzuki.s.0617@gmail.com', '@');
+$a = "きょUはぴIえIちぴIのくみこみかんすUのがくしゅUをしてIます";
+echo "変換前<br>", $a, "<br>";
+echo "変換後<br>";
+
+$b = array('I', 'U');
+$c = array('い', 'う');
+
+echo str_replace($b, $c, $a);
+
  ?>
