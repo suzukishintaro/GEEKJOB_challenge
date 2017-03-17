37712db7ed3357831ea097aa79d7647a1cd893e8
SHINTAROPC\shintaro <suzuki.s.0617@gmail.com>
2017-03-17 10:48:25 +0900

    グローバル変数

--- CampChallenge_PHP/001.プログラミング基礎学習１/php2.php
+++ CampChallenge_PHP/001.プログラミング基礎学習１/php2.php
@@ -1,18 +1,14 @@
 <?php
-$prof1 = array(1,  'kazuaki', '1988.01.01', 'kurodawara');
-$prof2 = array(2, 'masato', '1988.02.02', 'kawaguchi');
-$prof3 = array(3, 'takashi', '2088.03.03', 'kawagoe');
-
-$proflist = array($prof1, $prof2, $prof3);
-
-$keyword = "masato";
-
-for ($i=0; $i < count($proflist); $i++) {
-  for ($j=0; $j < count($proflist[$i]); $j++) {
-    if (array_search($keyword, $proflist[$i]) !== false) {
-       print_r($proflist[$i]);
-       break;
-    }
-  }
+$start_num = 3;
+function doubule(){
+  global $start_num;
+   $start_num *= 2;
+   
+   static $kaiten = 0;
+   $kaiten++;
 }
+for ($i=0; $i < 20; $i++) {
+  doubule();
+}
+echo $start_num;
  ?>
